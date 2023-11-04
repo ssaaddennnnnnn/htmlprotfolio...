7cc53e1bdf1f6276354de07ca6155b45cde7d7c0
ssaaddennnnnnn <149859638+ssaaddennnnnnn@users.noreply.github.com>
2023-11-04 21:09:14 +0300

    Add files via upload

---
+++ c+ 1-4.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+
+using namespace std;
+void main()
+{ 
+	short x = 200;
+	int y = 1000000;
+	double c = 10.47;/* double for not int numprs*/
+	char k = 'p';
+	string v = "sa";
+	bool l = false; /* الكومبايلر بتعامل مع الترو كأنها رقم 1 والفولس رقم صفر*/
+	
+ cout << " l = " << l;
+ cout << v << endl;
+ cout << 'p' << endl;
+ cout << "c=" << c << endl;
+ cout << x << endl;/* without douple koteshin when i need a valuy for x*/
+ cout << "\"sadeen\"" << endl;
+ cout << "\\abeer\\"<<endl;
+
+
+ 
+// بدنا نحكي هسا عن العمليات الحسابيه تمام
+
+ cout<<9%8<< endl;
+
+}
